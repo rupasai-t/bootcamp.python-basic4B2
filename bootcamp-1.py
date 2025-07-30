677c1002fe43fafcd419e7d38102a08770a361a6
rupasai-t <thotarupasai@gmail.com>
2025-07-30 13:48:26 +0530

    uploading bootcamp-1.py

---
+++ bootcamp-1.py
@@ -0,0 +1,14 @@
+name="MLEW"
+number=10.0
+isON=True
+letter="H"
+#number-=10
+# Arithemetic - + ,-, *, /, %, =
+#logic -and,or,not
+# comparison- <,>,==,!=,<=,=>
+# assignment-=,+=,-=,/=
+if (number >5):
+    print(name)
+else:
+    print(isON)
+    

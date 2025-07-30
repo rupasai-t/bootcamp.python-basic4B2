0cd21463b34d6a2421139e6f16acb817e3cda17f
rupasai-t <thotarupasai@gmail.com>
2025-07-30 13:49:57 +0530

    adding all files

---
+++ Assignment day-1.2.py
@@ -0,0 +1,11 @@
+number=10
+score=[10,20,30,40,50]
+names=["divya","hema","deepu","rupa","pallavi"]
+for i in range(number): 
+    print(i)
+
+for score in score: 
+    print(score)
+
+for name in names: 
+    print(name)
---
+++ Assignment day-1.3.py
@@ -0,0 +1,9 @@
+def greeting():
+     print("hello world") #function parameters example
+          
+greeting()
+
+def greeting(name):
+    print("hello world" +name)
+          
+greeting("puji")
